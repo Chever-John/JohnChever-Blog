138804b86e6d202248909ed2c2c743a0140384b9
CheverJohn <cheverjonathan@gmail.com>
2021-11-13 00:51:47 -0800

    Docs: fledgling website

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import clsx from 'clsx';
 import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
+import { Typography, Grid, Button } from "@material-ui/core";
+import { useTrail, animated, useSpring } from "react-spring";
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
 import HomepageFeatures from '../components/HomepageFeatures';
+import Translate from "@docusaurus/core/lib/client/exports/Translate";
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const context = useDocusaurusContext();
+  const { siteConfig = {} } = context;
+
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
@@ -20,14 +24,47 @@ function HomepageHeader() {
 
 export default function Home() {
   const {siteConfig} = useDocusaurusContext();
+  const animatedTexts = useTrail(5, {
+    from: { opacity: 0, transform: "translateY(3em)" },
+    to: { opacity: 1, transform: "translateY(0)" },
+    config: {
+      mass: 3,
+      friction: 45,
+      tension: 460,
+    },
+    delay: 200,
+  });
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
+      description="Description will go into a meta tag in <head />"
+    >
+    <Grid container spacing={2} style={{ padding: "5%" }} className="hero">
+      <Grid item xs={12} lg={6} className="homeIntro">
+        <animated.div style={animatedTexts[0]}>
+          <Typography variant="h2" gutterBootom>
+            <Translate>Hello! I am</Translate>
+            <span className="intro__name"> CheverJohn </span>
+          </Typography>
+        </animated.div>
+        <animated.div style = {animatedTexts[1]}>
+          <Typography variant="body1">
+            <Translate>
+              A University graduate who has a great passion for Tech.
+              While keeping updated with the most recent
+              technologies, I always seek to improve and grow as a
+              professional full-stack web developer as well as a person.
+            </Translate>{" "}
+          </Typography>
+        </animated.div>
+        &nbsp;
+
+      </Grid>
+    </Grid>
+      {/*<HomepageHeader />*/}
+      {/*<main>*/}
+      {/*  <HomepageFeatures />*/}
+      {/*</main>*/}
     </Layout>
   );
 }
