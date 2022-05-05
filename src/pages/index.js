976585480b6f2cc5fc6f86d35b2d99d39a19c765
CheverJohn <cheverjonathan@gmail.com>
2022-05-05 21:25:15 +0800

    website: add my resume

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import { Typography, Grid, Button } from "@material-ui/core";
 import { useTrail, animated, useSpring } from "react-spring";
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
-import resume from "../../static/files/CV_zh.pdf";
+import resume from "../../static/files/JohnChever_Software_Resume.pdf";
 import Translate from "@docusaurus/core/lib/client/exports/Translate";
 import useBaseUrl from "@docusaurus/core/lib/client/exports/useBaseUrl";
 
